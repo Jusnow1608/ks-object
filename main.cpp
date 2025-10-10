afc6d8de16a97ab237dd33443821b7e6add66c04
Justyna <justyna.nowak.programista@gmail.com>
2025-10-10 20:14:35 +0200

    Added default values in class Adresat

--- main.cpp
+++ main.cpp
@@ -8,17 +8,47 @@ int main()
     KsiazkaAdresowa ksiazkaAdresowa("Uzytkownicy.txt", "Adresaci.txt");
     ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
     ksiazkaAdresowa.rejestracjaUzytkownika();
-    ksiazkaAdresowa.rejestracjaUzytkownika();
     ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
     ksiazkaAdresowa.logowanieUzytkownika();
-    ksiazkaAdresowa.wyswietlWszystkichAdresatow();
     ksiazkaAdresowa.zmianaHaslaZalogowanegoUzytkownika();
     ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
+    ksiazkaAdresowa.wyswietlWszystkichAdresatow();
     ksiazkaAdresowa.dodajAdresata();
     ksiazkaAdresowa.wyswietlWszystkichAdresatow();
     ksiazkaAdresowa.wylogowanieUzytkownika();
-    ksiazkaAdresowa.dodajAdresata();
-    ksiazkaAdresowa.wyswietlWszystkichAdresatow();
+
+    return 0;
+}
+
+
+//Testy klasy AdresatMenadzer
+
+#include "AdresatMenadzer.h"
+
+using namespace std;
+
+int TestyAdresatMenadzer_main()
+{
+    AdresatMenadzer adresatMenadzer("Adresaci.txt",3);
+    adresatMenadzer.wyswietlWszystkichAdresatow();
+    adresatMenadzer.dodajAdresata();
+    adresatMenadzer.wyswietlWszystkichAdresatow();
+
+    return 0;
+}
+
+//Testy klasy PlikZAdresatami
+
+#include "Adresat.h"
+#include "PlikZAdresatami.h"
+
+int TestyPlikZAdresatami_main()
+{
+    PlikZAdresatami plikZAdresatami("Adresaci-test.txt");
+    Adresat adresat(8,1, "Michal", "Nowak","6543 543 654", "jtrdds@wp.pl", "ul. Gleboka 19, m. 56, Lodz 95-345");
+    plikZAdresatami.dopiszAdresataDoPliku(adresat);
+    plikZAdresatami.pobierzAdresatowZalogowanegoUzytkownikaZPliku(1);
+    cout<<plikZAdresatami.pobierzIdOstatniegoAdresata();
 
     return 0;
 }
